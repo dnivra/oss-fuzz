b6a8a1a5360c7cb816c4308ea0b10f970a1d75f5
Arvind <dnivra@users.noreply.github.com>
2024-03-20 18:36:44 -0700

    Fix augeas harness build

--- projects/augeas/build.sh
+++ projects/augeas/build.sh
@@ -33,7 +33,7 @@ cp $SRC/augeas_api_fuzzer.cc .
 
 for fuzzer in augeas_api_fuzzer augeas_escape_name_fuzzer augeas_fa_fuzzer; do
     $CXX $CXXFLAGS -std=c++11 -Isrc/ `xml2-config --cflags` \
-        $fuzzer.cc -o $OUT/$fuzzer $LIB_FUZZING_ENGINE \
+        $fuzzer.cc -o $OUT/$fuzzer $LIB_FUZZING_ENGINE -lpthread \
         src/.libs/libaugeas.a src/.libs/libfa.a ./gnulib/lib/.libs/libgnu.a \
         /usr/lib/x86_64-linux-gnu/libxml2.a
 done
